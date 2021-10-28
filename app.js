2eae8b69d042dae9b06f8137d7b6439859760639
ReoNaBear <adamsimprove1108@gmail.com>
2021-10-28 13:01:05 +0800

    feat: login page and passort function

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const exphbs = require('express-handlebars');
 const app = express()
 const routes = require('./routes')
+const flash = require('connect-flash')
+const session = require('express-session')
+const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 
@@ -11,6 +14,12 @@ app.use(routes)
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
