30c6d3d8a7dbdaa44630c3b5b6cd5acaa7be229d
ReoNaBear <adamsimprove1108@gmail.com>
2021-10-28 14:38:58 +0800

    feat: add user authentication in todo and home routers

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const exphbs = require('express-handlebars');
+const session = require('express-session')
 const app = express()
 const routes = require('./routes')
 const flash = require('connect-flash')
-const session = require('express-session')
+
 const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 
 
 
-app.use(routes)
+
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -20,6 +21,19 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+
+usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  console.log(req.user)
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  next()
+})
+
+app.use(routes)
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
+const { authenticator } = require('../middleware/auth')
 const users = require('./modules/users')
 const auth = require('./modules/auth')
 
-router.use('/', home)
+
 router.use('/users', users)
 router.use('/auth', auth)
+
+router.use('/', authenticator, home)
 
 module.exports = router
