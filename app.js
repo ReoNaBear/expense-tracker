18d57443afae187e3a4ad8a753297d528428fc0c
ReoNaBear <adamsimprove1108@gmail.com>
2021-10-28 14:59:14 +0800

    feat: create flash message

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use((req, res, next) => {
   console.log(req.user)
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
-  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
-  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
